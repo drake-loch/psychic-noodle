978e7a2d121bae95c7a644d912c569be599bf7f0
drake <tehtrojenguy@gmail.com>
2021-06-19 18:31:18 -0600

    added styling to post browser

--- src/components/Pages/PostBrowser/PostBrowser.jsx
+++ src/components/Pages/PostBrowser/PostBrowser.jsx
@@ -10,7 +10,7 @@ class PostData {
         this.name = name;
         this.now = new Date();
         this.timestamp = {
-            day: this.now.getDay(),
+            day: this.now.getDate(),
             month: this.now.getMonth(),
             year: this.now.getFullYear(),
             time: this.now.getTime(),
@@ -22,12 +22,13 @@ class PostData {
 
 function PostBrowser() {
 
-    const {createPost, getAllPosts} = useAuth();
+    const {createPost, getAllPosts, currentUser} = useAuth();
     const [postList, setPostList] = useState([]);
     const [renderList, setRenderList] = useState(false);
 
     //=============temp=============
-    let tempData = new PostData("A title","drake loch","Twas two weeks before christmas");
+    let tempLorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sapiente facilis eveniet neque. Nihil voluptates nam id dolore voluptatibus blanditiis et quibusdam, delectus, necessitatibus, dolorum nostrum fugit consequuntur officiis autem.";
+    let tempData = new PostData("A title","drake loch",tempLorem);
     //==============================
 
     useEffect(() => {
@@ -52,7 +53,7 @@ function PostBrowser() {
         let toReturn = [];
         console.log("TO render:");
         console.log(toRender);
-        toRender.forEach(post => toReturn.push(<PostCard />));
+        toRender.forEach(post => toReturn.push(<PostCard title={post.data.title} name={post.data.name} timestamp={post.data.timestamp} body={post.data.body} />));
         // toReturn = <PostCard />
         return toReturn;
     }
@@ -61,10 +62,10 @@ function PostBrowser() {
         createPost(tempData);
     }
     return (
-        <div className="w-screen h-screen bg-gray-400">
+        <div className="w-screen min-h-screen pb-5 bg-gray-400">
             <Navbar />
-            <button onClick={e => addPost()}>temp button</button>
-            <section className="px-2 w-screen pt-10">
+            {currentUser ? <button onClick={e => addPost()} className="px-3 py-2 bg-green-500 border border-green-700 ml-5 mt-5">Create Post +</button> : null}
+            <section className="px-2 w-screen pt-5">
                 {renderList ? renderPostCards(postList) : null}
             </section>
         </div>
@@ -74,10 +75,20 @@ function PostBrowser() {
 export default PostBrowser
 
 
-export function PostCard() {
+export function PostCard({title,name,timestamp,body}) {
+
+    function timeConverter(){
+        return `${timestamp.month}/${timestamp.day}/${timestamp.year}`;
+    }
     return (
-        <div className="w-full h-20 bg-red-500 mb-2">
-            <p>hello there!</p>
+        <div className="bg-gray-300 w-full min-h-1/2 mb-2 flex flex-wrap px-5 pt-5 pb-10 shadow-md" style={{fontFamily:'OpenSans'}}>
+            <h4 className="w-full text-center font-bold" style={{fontFamily:'TitilliumWeb', fontSize:'1.75rem'}}>{title}</h4>
+            <div className="w-full flex">
+                <p className="w-1/2 py-2">By: {name}</p>
+                <p className="w-1/2 py-2 text-right">Posted: {timeConverter()}</p>
+            </div>
+            <p className="w-full border-t border-b py-2">{body}</p>
+            <p className="w-full text-right text-purple-600">Expore Further...</p>
         </div>
     )
 }
